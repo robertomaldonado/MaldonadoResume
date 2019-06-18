21f8f83504376cd30cc9d113ebafb6a05e8131c1
Roberto <robertomaldonadog@hotmail.com>
2019-06-18 12:37:55 -0400

    Send data to components

--- src/PersonalSiteDashboard.js
+++ src/PersonalSiteDashboard.js
@@ -1,30 +1,17 @@
 import React from 'react';
-// import AboutSection from './AboutSection';
-// import EducationSection from './EducationSection';
-import Detail from './Detail';
+import EducationSection from './EducationSection';
 import AboutSection from './AboutSection';
+import WorkExperienceSection from './WorkExperienceSection';
 
 export class PersonalSiteDashboard extends React.Component {
     
     render(){
-        // const {details} = this.props
-        // return (
-        //     <div> 
-        //         <Detail 
-        //             key={details.id} 
-        //             name={details.name}
-        //             phone={details.phone}
-        //             email = {details.email}
-        //             github={details.github}
-        //             linkedin={details.linkedin}
-        //         />
-        //     </div>
-        // );
-
-        const {details} = this.props
+        const {details, education} = this.props
         return (
             <div> 
                 <AboutSection details={details} />
+                <EducationSection education={education} />
+                <WorkExperienceSection />
             </div>
         );
     }
