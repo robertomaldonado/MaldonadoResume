844abd05ce1522f86f5a54ac3d6b4991906bc197
Roberto <robertomaldonadog@hotmail.com>
2019-06-18 23:07:27 -0400

    Added formatting for ui

--- src/EducationSection.js
+++ src/EducationSection.js
@@ -2,26 +2,50 @@ import React from "react";
 
 class EducationSection extends React.Component {
     render(){
+        const {education}=this.props
         return (
-
         <div className="ui text container">
             <div className="ui segment">
-                <div className="ui center aligned icon header">
-                <i class="graduation cap icon"></i>
-                    Education
-                </div>
-                <p >
-                    ...
+                <p className="large text"> 
+                {/* <div className="ui center aligned icon header"> */}
+                    <i class="large graduation cap icon"></i>
+                    <b> EDUCATION </b> 
                 </p>
-                <button
+                {education.map(
+                    (item, i) => 
+                    <div className="ui text segment">
+                    <div className="ui two column stackable center aligned grid">
+                      <div className="middle aligned row">
+                      <div className="column left aligned">
+                            <b> {item.institution} </b> <br />
+                            {item.studyType} <br />
+                            GPA: {item.gpa}/4.0
+                      </div>
+                      <div className="right aligned">
+                        {item.area} | {item.startDate} - {item.endDate} 
+                      </div>  
+                      </div>
+                    </div>
+                    </div>
+                )}
+                {/* <button
                 className="ui basic button icon">
                 <i className="plus icon" />
-                </button>
+                </button> */}
             </div>
         </div>
-
         );
     }
 }
 
 export default EducationSection;
+
+// "institution": "Universidad San Francisco de Quito",
+//           "area": "Computer Science",
+//           "studyType": "Bachelor",
+//           "startDate": "August 2010",
+//           "endDate": "November 2015",
+//           "gpa": "3.5",
+//           "courses": [
+//             "Basic SQL"
+//           ]
